c61afe09347b65ef3f5f16a87b71c882873c3a87
AWSUltramarine <walidahmm\@yahoo.com>
2025-03-02 20:05:13 -0600

    update

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def home():
     """Return a basic status message."""
     return jsonify({"status": "alive"}), 200
 
+
 if __name__ == "__main__":
     # Run on port 8080 for DAST (ZAP) testing
     app.run(host="0.0.0.0", port=8080, debug=False)
